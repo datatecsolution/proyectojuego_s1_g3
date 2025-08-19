0c67a155a4dc3d617547f293652973e40df125a0
Christian Joseth Rodriguez Aguilar <you@example.com>
2025-08-19 10:49:55 -0600

    Agregue Jugador

--- src/main/java/net/perlasjp/juego/Jugador.java
+++ src/main/java/net/perlasjp/juego/Jugador.java
@@ -1,6 +1,16 @@
 package net.perlasjp.juego;
 
 public class Jugador {
+    private String nombre;
+    private int salud;
+    private int forma;
+
+    public int atacar()
+    public void recibirDanio(danio:int)void
+    public boolean estaVivo()
+    public su
+    public 
+
     
     
 }
