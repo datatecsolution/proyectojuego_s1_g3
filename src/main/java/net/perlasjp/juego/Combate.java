2491958f293a0c8b893625d83c098a74264c3598
Yare Madrid <madridyarely6@gmail.com>
2025-08-19 10:12:59 -0600

    Combate

--- src/main/java/net/perlasjp/juego/Combate.java
+++ src/main/java/net/perlasjp/juego/Combate.java
@@ -1,5 +1,11 @@
 package net.perlasjp.juego;
 
 public class Combate {
-    
+
+    private Jugador jugador;
+    private Enemigo enemigo;
+
+    public void iniciar(jugador:jugador,enemigo:Enemigo){
+
+    }
 }
