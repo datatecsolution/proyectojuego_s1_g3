8dd3cd6d809b3e6207d538105fce3c068caafcc5
unknown <yasairatrigueros.com>
2025-08-19 10:27:09 -0600

    Enemigo

--- src/main/java/net/perlasjp/juego/Enemigo.java
+++ src/main/java/net/perlasjp/juego/Enemigo.java
@@ -2,4 +2,27 @@ package net.perlasjp.juego;
 
 public class Enemigo {
     
+    private String nombre;
+    private int salud;
+    private int fuerza;
+
+
+    public int atacar(){
+
+    }
+    public void recibirDanio(daño:int){
+
+    }
+    public boolean estarVivo(){
+
+    }
+    public Enemigo (Strin, int){
+
+    }
+    public  String gitNombre{
+
+    }
+    public int gitSalud{
+
+    }
 }
