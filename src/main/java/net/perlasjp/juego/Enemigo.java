a2f8e49e4ba139ac1f9877997cbc4eb9640139d2
unknown <yasairatrigueros.com>
2025-08-25 19:54:27 -0600

    Enemigo

--- src/main/java/net/perlasjp/juego/Enemigo.java
+++ src/main/java/net/perlasjp/juego/Enemigo.java
@@ -6,20 +6,30 @@ public class Enemigo {
     private int salud;
     private int fuerza;
 
+    public void recibirDanio(int cantidad){
+        salud = salud-cantidad;
+        if (salud <0) salud=0;
+    }
 
     public int atacar(){
-        return 0;
-
-    }
-    public void recibirDanio(int danio){
+        return fuerza;
 
     }
     public boolean estarVivo(){
-        return false;
+        return salud>0;
+    }
+    public Jugador(String nombre){
+        this.nombre = nombre;
+        this.salud = 100;
+        this.fuerza =10;
     }
     public Enemigo (int salud, String n){
 
-
+public void mostrarEstado(){
+    System.out.println("Jugador:" + nombre);
+     System.out.println("Salud:" + Salud);
+      System.out.println("Ataque:" + fuerza);
+}
     }
     public  String getNombre(){
         return nombre;
